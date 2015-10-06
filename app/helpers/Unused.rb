28c94aa60ef6b57a713375b86022dd0a0e65261a
XanderPSON <xwpeterson@gmail.com>
2015-10-05 20:50:13 -0700

    Add working buttons for kanji

---
+++ app/helpers/Unused.rb
@@ -0,0 +1,4 @@
+<script src="https://fb.me/react-0.13.3.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
+  <script type="text/babel"></script>
+  <div id="react-container"></div></head>
